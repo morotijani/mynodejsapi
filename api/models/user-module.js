a2658048c7f3a14d94d2a58f9b9fba89d4fb921f
Moro Tijani <tjhackx111@gmail.com>
2022-06-15 06:37:14 +0000

    user create and update

--- api/models/user-module.js
+++ api/models/user-module.js
@@ -1,11 +1,12 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const mysql = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const {Sequelize, DataTypes} = require('sequelize');
+const bcrypt = require('bcrypt');
+const mysql = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
     host: process.env.DB_HOST,
     dialect: 'mysql',
     pool: {min: 0, max: 5}
 });
 
-const User = mysql.define('User', {
+const User = mysql.define('User',{
     fname: {
         type: DataTypes.STRING
     },
@@ -16,17 +17,15 @@ const User = mysql.define('User', {
         type: DataTypes.STRING(155),
         unique: true,
         validate: {
-            isEmail: {
-                msg: "Must be a valid email."
-            }
-        },
-        password: {
-            type: DataTypes.STRING(75),
-            validate: {
-                len: {
-                    msg: "Password must be at least 8 characters.",
-                    args: [8, 255]
-                }
+            isEmail: {msg: "Must be a valid email."}
+        }
+    },
+    password: {
+        type: DataTypes.STRING(75),
+        validate: {
+            len: {
+                msg: "Password must be at least 8 characters.",
+                args: [8, 255]
             }
         }
     }
@@ -37,5 +36,17 @@ const User = mysql.define('User', {
 });
 
 User.sync();
+
+User.beforeCreate(async (user, options) => {
+    const hashed = await bcrypt.hash(user.password, 10);
+    user.password = hashed;
+});
+
+User.beforeSave(async (user, options) => {
+    if(user.updatePassword) {
+        const hashed = await bcrypt.hash(user.password, 10);
+        user.password = hashed;
+    }
+})
 
 module.exports = User;
